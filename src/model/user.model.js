8f2762880bf2636eefd69d5c982f27e0af024dfd
Praman Bhogal <pramanbhogal13@gmail.com>
2026-05-29 21:14:31 +0530

    feat: create User model with validation constrains

---
+++ src/model/user.model.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+// Swapped Regex for the 'validator' package to enforce production-ready email verification
+// and to experiment and learning new things 
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        trim:true,
+        required:[true ,"Name field is required"],
+        minlength:[2 ,"Name must be at least 2 characters long"],
+        maxlength:[50 ,"Name cannot exceed 50 characters"],
+    },
+    email:{
+        type:String,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        required:[true,"Email field is required"],
+        validate:[validator.isEmail , "Please enter correct email address"]
+        //no minlength or maxlength because validator package handles it  
+    },
+    mobile:{
+        type:String,
+        required:[true,"Mobile field is required"],
+        trim:true,
+        validate:[(value) => validator.isMobilePhone(value , "any" , {strictMode:true}) ,
+            "Please provide a valid international phone number"
+        ]
+    },
+    password: {
+    type: String,
+    required: [true, "Password field is required"],
+    minlength: [8, "Password must be at least 8 characters long"],
+    select: false // act as .select('-password')
+  }
+})
+
+const User = mongoose.model('User',userSchema); //User automatically becomes user lowercased by mongodb /mongoose
+module.exports = User;
