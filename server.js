4433bed332c07c37d8e18a31ea780772659ae425
sharatkatwa <sjkatwa2000@gmail.com>
2026-06-18 12:16:36 +0530

    CHANGE: app.listen now serve as a function for imporved reusability

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const app = require('./src/app');
-const connectDB = require('./src/config/db');
-const PORT = process.env.PORT || 4000
-connectDB();
-app.listen(PORT ,() => {
-    console.log("server is running on port",PORT);
-})
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
+const PORT = process.env.PORT || 4000;
+
+const startServer = () => {
+  connectDB().then(() => {
+    app
+      .listen(PORT, () => {
+        console.log("server is running on port", PORT);
+      })
+      .catch((err) => {
+        console.log("error while running the server", err);
+      });
+  });
+};
